72fd961b02189017068e4e3bc075dcd04d746acd
KUAN-YING CHOU <kuanyingchou@gmail.com>
2014-11-15 00:52:32 +0800

    initial commit

---
+++ Assets/Editor/TestCalculator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class TestCalculator {
+
+	[Test]
+	public void TestAdd(){
+		Assert.That (Calculator.Add(1, 1) == 2);
+	}
+		
+	[Test]
+	public void TestAddFloat(){
+
+		Assert.That((1.0001f+2.2221f),Is.EqualTo(Calculator.Add(1.0001f,2.2221f)));
+	}
+
+	[Test]
+	public void TestSub()
+	{
+		Assert.That (Calculator.Sub(1, 1) == 0);
+		Assert.That (Calculator.Sub(2, 1) == 1);
+	}
+
+	[Test]
+	public void TestSumValues ( ){
+		Assert.That (Calculator.Add( new int[]{ 1,2,3 }  ) == 6);
+		Assert.That (Calculator.Add( new int[]{ 1,2,5 }  ) == 8);
+	}
+
+	[Test]
+	public void TestSubstractValues ( ){
+		Assert.That(-4,Is.EqualTo(Calculator.Sub(new int[]{1,2,3})));
+		Assert.That (Calculator.Sub(new int[0]) == 0);
+
+		Assert.That(0,Is.EqualTo(Calculator.Sub(null)));
+		Assert.That(6,Is.EqualTo(Calculator.Mul(new int[]{1,2,3})));
+
+	}
+
+
+}
---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public static class Calculator{
+
+
+
+	public static int Add(int a, int b)
+	{
+		return a + b;
+	}
+
+	public static float Add(float a, float b)
+	{
+		return a + b;
+	}
+
+	public static int Sub(int a,int b){
+		return a - b;
+	}
+
+	public static float Sub(int[] values){
+
+		return Calculate ((a,b)=>a-b ,values);
+	}
+
+
+	public static int Add(int[] values) {
+		return Calculate ((a,b)=>a+b ,values);
+
+	}
+
+	public static int Mul(int[] values) {
+		return Calculate ((a,b)=>a*b ,values);
+		
+	}
+
+
+
+
+	public static int Calculate(Func<int,int,int> func,int[] values){
+
+		if (values == null||values.Length == 0)
+			return 0;
+		
+		int result = values [0];
+		for (int i=1; i<values.Length; i++) {
+			result=func(result,values[i]);
+		}
+		return result;
+		
+	}
+}
